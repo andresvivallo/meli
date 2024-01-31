6971990d2b317deeff1de5c3493cc6955f57e753
andresvivallo <vivallo.cl@gmail.com>
2024-01-31 19:52:30 -0300

    last updates

--- main.py
+++ main.py
@@ -1,160 +1,61 @@
 import os
+import time
 import warnings
-from datetime import timedelta
 
-from config import JSON_CONFIG
 from utils import (
     unzipping_all_files,
     trf_json_file_to_dataframe,
     join_prints_with_taps,
-    read_pays_csv,
+    calculate_all_metrics
 )
+from config import JSON_CONFIG
 
 ZIP_FILE = "CodeEx.zip"
 DEST_PATH = "tmp"
 PAY_FILE = "pays.csv"
 
 
-
 if __name__ == "__main__":
+    start_time = time.time()
     warnings.filterwarnings("ignore")
     wd = os.getcwd()
 
+    # Unzipping files from .zip
     unzipping_all_files(
         file=f"{wd}/data/{ZIP_FILE}",
         dest_path=f"{wd}/{DEST_PATH}/"
     )
     
+    # Transform json files to python dataframes
     df_dict = trf_json_file_to_dataframe(
         path_files=f"{wd}/{DEST_PATH}/{ZIP_FILE.replace('.zip', '')}",
         json_config=JSON_CONFIG
     )
     
+    # Transform json files to python dataframes
     prints_with_taps = join_prints_with_taps(
         df_dict=df_dict
     )
 
-
-    import pandas as pd
-    # Getting value_props
-    lst_value_prop = list(prints_with_taps.value_prop.unique())
-    
-    # List of useful new columns
-    # pre_three_lw_prints = "three_lw_prints_"
-    # three_lw_prints_colums = [
-    #     pre_three_lw_prints + sub for sub in lst_value_prop
-    # ] 
-    
-    # Considering the last week as the last 7 days recorded in the data
-    start_date_of_last_week = (
-        prints_with_taps.day.max() - timedelta(days = 6)
-    )
-    
-    # Filtering only the last week prints
-    prints_last_week = prints_with_taps.loc[
-        prints_with_taps.day >= start_date_of_last_week
-    ].reset_index()
-
-    # Adding new columns
-    # prints_last_week[three_lw_prints_colums] = 0
-    
-    # Creating empty dataframe for save counts
-    three_lw_result = pd.DataFrame()
-
-    # Working by users
-    users_ids = list(prints_last_week.user_id.unique())
-    for i in range(len(users_ids)):
-        user_id = users_ids[i]
-        print()
-        print(f"Working on user {i} of {len(users_ids)-1}")
-        # Getting unique days of specific user
-        days = list(
-            prints_last_week[
-                prints_last_week.user_id == user_id
-            ].day.unique()
+    final_result = calculate_all_metrics(
+        prints_with_taps=prints_with_taps,
+        path_pays_csv=(
+            f"{wd}/{DEST_PATH}/"
+            f"{ZIP_FILE.replace('.zip', '')}/{PAY_FILE}"
         )
-        
-        # Loop into unique days of the user
-        for k in range(len(days)):
-            day = days[k]
-            
-            # Defining window of analyze, considering the 
-            # last 3 weeks (21 days) before the objective row
-            end_date = day - timedelta(days=1)
-            start_date = (
-                end_date - timedelta(days=20)
-            )
-            # Filtering with the window of analyze and user_id
-            mask = (
-                prints_with_taps.day >= start_date
-            ) & (
-                prints_with_taps.day <= end_date
-            ) & (
-                prints_with_taps.user_id == user_id
-            )
-
-            # Applying filter and counting prints by value_prop
-            counts = prints_with_taps.loc[mask].groupby(
-                "value_prop"
-            ).count().reset_index()[
-                ["value_prop", "day"]
-            ].rename(columns={"day": "count"})
-            # adding useful info
-            counts["user_id"] = user_id
-            counts["day"] = day
-            
-            three_lw_result = pd.concat([three_lw_result, counts])
-
-
-        # Defining last 3 weeks prints counts by row and value_prop
-        # for three_lw_prints in three_lw_prints_colums:
-        #     try:
-        #         value = counts[
-        #             counts["value_prop"] == (
-        #                 f"{three_lw_prints.replace(pre_three_lw_prints, '')}"
-        #             )
-        #         ]["count"]
-        #         prints_last_week[f"{three_lw_prints}"][i] = value
-        #     except:
-        #         prints_last_week[f"{three_lw_prints}"][i] = 0
-
-    # long to wide and rename columns
-    prefix = "l3w_prints"
-    three_lw_result_wide = three_lw_result.pivot(
-        index=["day", "user_id"],
-        columns=["value_prop"],
-        values="count"
-    ).reset_index().rename(
-        columns={
-            "cellphone_recharge": f"{prefix}_cellphone_recharge",
-            "credits_consumer" : f"{prefix}_credits_consumer",
-            "link_cobro" : f"{prefix}_link_cobro",
-            "point" : f"{prefix}_point",
-            "prepaid" : f"{prefix}_prepaid",
-            "send_money" : f"{prefix}_send_money",
-            "transport" : f"{prefix}_transport",
-        }
-    )
-    three_lw_result_wide.fillna(0, inplace=True)
-
-    prints_last_week_and_l3w = prints_last_week.merge(
-        three_lw_result_wide,
-        on=["day", "user_id"],
-        how="left"
     )
 
-
-
-
-
-
-    
-    df_pays = read_pays_csv(
-            path_file=(
-                f"{wd}/{DEST_PATH}/"
-                f"{ZIP_FILE.replace('.zip', '')}/{PAY_FILE}"
-            )
-        )
-
-    
-    
+    # Print process time and file results paths
+    process_time = round(
+        (time.time() - start_time)/60,
+        2
+    )
+    print()
+    print(
+        f"All process was completed successfully in "
+        f"{process_time} minutes, "
+        f"you can see the result dataframe in the file: "
+        f"{final_result.get('df_result_file')}, and "
+        f"a descriptive table in the following file: "
+        f"{final_result.get('df_result_describe_file')}"
+    )
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+import os
 import zipfile
 import pandas as pd
+
+from datetime import timedelta
 
 
 def unzipping_all_files(
@@ -115,11 +118,276 @@ def join_prints_with_taps(
 
     # Returning the resulting dataframe
     return prints_with_tap
+    
 
+def calculate_all_metrics(
+        prints_with_taps: pd.DataFrame,
+        path_pays_csv: str
+) -> dict[str, pd.DataFrame]:
+    print()
+    print(
+        f"Calculating all necessary metrics, "
+        f"this process take a few minutes.."
+    )
+    # Necessary value for printed process logic
+    process_percent_printed = 0
+    # Read pays.csv
+    df_pays = pd.read_csv(path_pays_csv)
+    # To datetime
+    df_pays["pay_date"] = pd.to_datetime(df_pays["pay_date"])
+    
+    # Considering the last week as the last 7 days recorded in the data
+    start_date_of_last_week = (
+        prints_with_taps.day.max() - timedelta(days = 6)
+    )
+    
+    # Filtering only the last week prints
+    prints_last_week = prints_with_taps.loc[
+        prints_with_taps.day >= start_date_of_last_week
+    ].reset_index()
+    
+    # Creating empty dataframes for save results
+    three_lw_prints_result = pd.DataFrame()
+    three_lw_taps_result = pd.DataFrame()
+    three_lw_pays_counts_result = pd.DataFrame()
+    three_lw_pays_total_result = pd.DataFrame()
 
-def read_pays_csv(
-        path_file: str
-    ) -> pd.DataFrame:
-    # Returning dataframe from .csv file
-    return pd.read_csv(path_file)
+    # Working by users
+    users_ids = list(prints_last_week.user_id.unique())
+    for i in range(len(users_ids)):
+        user_id = users_ids[i]
+        process_percent = round(i/(len(users_ids)-1)*100)
+        if process_percent in list(range(2, 102, 2)):
+            if process_percent != process_percent_printed:
+                print(f"{process_percent}% of the process done!")
+                process_percent_printed = process_percent
+            
+                
+        
+        # Getting unique days of specific user
+        days = list(
+            prints_last_week[
+                prints_last_week.user_id == user_id
+            ].day.unique()
+        )
+        # Loop into unique days of the user
+        for k in range(len(days)):
+            day = days[k]
+            
+            # Defining window of analyze, considering the 
+            # last 3 weeks (21 days) before the objective row
+            end_date = day - timedelta(days=1)
+            start_date = (
+                end_date - timedelta(days=20)
+            )
+            
+            # Filtering prints with the window of analyze and user_id
+            mask = (
+                prints_with_taps.day >= start_date
+            ) & (
+                prints_with_taps.day <= end_date
+            ) & (
+                prints_with_taps.user_id == user_id
+            )
+            
+            # Filtering pays with the window of analyze and user_id
+            mask_pays = (
+                df_pays.pay_date >= start_date
+            ) & (
+                df_pays.pay_date <= end_date
+            ) & (
+                df_pays.user_id == user_id
+            )
+
+            # Applying filter in prints and pays DF
+            prints_with_taps_filter = prints_with_taps.loc[mask]
+            df_pays_filter = df_pays.loc[mask_pays]
+            
+            # Counting prints by value_prop
+            counts_prints = prints_with_taps_filter.groupby(
+                "value_prop"
+            ).count().reset_index()[
+                ["value_prop", "day"]
+            ].rename(columns={"day": "count"})
+            # adding useful info
+            counts_prints["user_id"] = user_id
+            counts_prints["day"] = day
+
+            # Counting taps by value_prop
+            counts_taps = prints_with_taps_filter[
+                prints_with_taps_filter["tap"]
+            ].groupby(
+                "value_prop"
+            ).count().reset_index()[
+                ["value_prop", "day"]
+            ].rename(columns={"day": "count"})
+            # adding useful info
+            counts_taps["user_id"] = user_id
+            counts_taps["day"] = day
+
+            # Counting pays by value_prop
+            counts_pays = df_pays_filter.groupby(
+                "value_prop"
+            ).count().reset_index()[
+                ["value_prop", "pay_date"]
+            ].rename(columns={"pay_date": "count"})
+            # adding useful info
+            counts_pays["user_id"] = user_id
+            counts_pays["day"] = day
+
+            # Sum total pays by value_prop
+            total_pays = df_pays_filter.groupby(
+                "value_prop"
+            ).sum("total").reset_index()[
+                ["value_prop", "total"]
+            ].rename(columns={"pay_date": "count"})
+            # adding useful info
+            total_pays["user_id"] = user_id
+            total_pays["day"] = day
+            
+            # Saving results
+            three_lw_prints_result = pd.concat(
+                [
+                    three_lw_prints_result,
+                    counts_prints
+                ]
+            )
+            three_lw_taps_result = pd.concat(
+                [
+                    three_lw_taps_result,
+                    counts_taps
+                ]
+            )
+            three_lw_pays_counts_result = pd.concat(
+                [
+                    three_lw_pays_counts_result,
+                    counts_pays
+                ]
+            )
+            three_lw_pays_total_result = pd.concat(
+                [
+                    three_lw_pays_total_result,
+                    total_pays
+                ]
+            )
+    
+    # Long to wide and rename columns of all results
+    prefix_prints = "l3wprints"
+    prefix_taps = "l3wtaps"
+    prefix_count_pays = "l3wcountpays"
+    prefix_total_pays = "l3wtotalpays"
+
+    # Prints 
+    three_lw_prints_result_wide = three_lw_prints_result.pivot(
+        index=["day", "user_id"],
+        columns=["value_prop"],
+        values="count"
+    ).reset_index().rename(
+        columns={
+            "cellphone_recharge": f"{prefix_prints}_cellphone_recharge",
+            "credits_consumer" : f"{prefix_prints}_credits_consumer",
+            "link_cobro" : f"{prefix_prints}_link_cobro",
+            "point" : f"{prefix_prints}_point",
+            "prepaid" : f"{prefix_prints}_prepaid",
+            "send_money" : f"{prefix_prints}_send_money",
+            "transport" : f"{prefix_prints}_transport",
+        }
+    )
+
+    # Taps
+    three_lw_taps_result_wide = three_lw_taps_result.pivot(
+        index=["day", "user_id"],
+        columns=["value_prop"],
+        values="count"
+    ).reset_index().rename(
+        columns={
+            "cellphone_recharge": f"{prefix_taps}_cellphone_recharge",
+            "credits_consumer" : f"{prefix_taps}_credits_consumer",
+            "link_cobro" : f"{prefix_taps}_link_cobro",
+            "point" : f"{prefix_taps}_point",
+            "prepaid" : f"{prefix_taps}_prepaid",
+            "send_money" : f"{prefix_taps}_send_money",
+            "transport" : f"{prefix_taps}_transport",
+        }
+    )
+     
+    # Count Pays
+    three_lw_pays_counts_result_wide = three_lw_pays_counts_result.pivot(
+        index=["day", "user_id"],
+        columns=["value_prop"],
+        values="count"
+    ).reset_index().rename(
+        columns={
+            "cellphone_recharge": f"{prefix_count_pays}_cellphone_recharge",
+            "credits_consumer" : f"{prefix_count_pays}_credits_consumer",
+            "link_cobro" : f"{prefix_count_pays}_link_cobro",
+            "point" : f"{prefix_count_pays}_point",
+            "prepaid" : f"{prefix_count_pays}_prepaid",
+            "send_money" : f"{prefix_count_pays}_send_money",
+            "transport" : f"{prefix_count_pays}_transport",
+        }
+    )
+
+    # Total Pays
+    three_lw_pays_total_result_wide = three_lw_pays_total_result.pivot(
+        index=["day", "user_id"],
+        columns=["value_prop"],
+        values="total"
+    ).reset_index().rename(
+        columns={
+            "cellphone_recharge": f"{prefix_total_pays}_cellphone_recharge",
+            "credits_consumer" : f"{prefix_total_pays}_credits_consumer",
+            "link_cobro" : f"{prefix_total_pays}_link_cobro",
+            "point" : f"{prefix_total_pays}_point",
+            "prepaid" : f"{prefix_total_pays}_prepaid",
+            "send_money" : f"{prefix_total_pays}_send_money",
+            "transport" : f"{prefix_total_pays}_transport",
+        }
+    )
+
+    # Fill NaN with 0
+    three_lw_prints_result_wide.fillna(0, inplace=True)
+    three_lw_taps_result_wide.fillna(0, inplace=True)
+    three_lw_pays_counts_result_wide.fillna(0, inplace=True)
+    three_lw_pays_total_result_wide.fillna(0, inplace=True)
+
+    # Merging all dataframes
+    prints_last_week_with_l3w_data = prints_last_week.merge(
+        three_lw_prints_result_wide,
+        on=["day", "user_id"],
+        how="left"
+    ).merge(
+        three_lw_taps_result_wide,
+        on=["day", "user_id"],
+        how="left"
+    ).merge(
+        three_lw_pays_counts_result_wide,
+        on=["day", "user_id"],
+        how="left"
+    ).merge(
+        three_lw_pays_total_result_wide,
+        on=["day", "user_id"],
+        how="left"
+    )
+
+    # Fill NaN in df_result and save results in CSV
+    prints_last_week_with_l3w_data.fillna(0, inplace=True)
+    df_result = prints_last_week_with_l3w_data
+    df_result_describe = df_result.describe()
+    df_result.to_csv(
+        f"{os.getcwd()}/df_result.csv"
+    )
+    prints_last_week_with_l3w_data.describe().to_csv(
+        f"{os.getcwd()}/df_result_describe.csv"
+    )
+
+    # Create dict to return results
+    result = {
+        "df_result": df_result,
+        "df_result_describe": df_result_describe,
+        "df_result_file": f"{os.getcwd()}/df_result.csv",
+        "df_result_describe_file": f"{os.getcwd()}/df_result_describe.csv"
+    }
+
+    return result
     
